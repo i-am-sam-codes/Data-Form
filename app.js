04d2fa3cfd4831ee13871fb45e0a254d9dc4a74a
Sam Ceja <sammmceja@gmail.com>
2023-08-03 18:40:29 -0700

    add email error message

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
 const form = document.getElementById("form");
 const subBtn = document.getElementById("sub-btn");
+const emailInput = document.getElementById("email");
+const emailError = document.querySelector("#email + span.error");
+const countryInput = document.getElementById("country");
+const zipCodeInput = document.getElementById("zip-code");
+const passwordInput = document.getElementById("password");
+const confirmInput = document.getElementById("password-confirmation");
 
 //form validation
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  subBtn.addEventListener("click", displayPopup);
+  if (!emailInput.validity.valid) {
+    showError();
+    e.preventDefault();
+  }
 });
+
+emailInput.addEventListener("input", (e) => {
+  if (emailInput.validity.valid) {
+    emailError.textContent = ""; // Clear the error message
+    emailError.classList.remove("active"); // Hide the error box
+  } else {
+    showError();
+  }
+});
+
+function showError() {
+  if (emailInput.validity.valueMissing) {
+    emailError.textContent = "You need to enter an email address.";
+  } else if (emailInput.validity.typeMismatch) {
+    emailError.textContent = "Please enter a valid email address.";
+  } else if (emailInput.validity.tooShort) {
+    emailError.textContent = `Email should be at least ${emailInput.minLength} characters; you entered ${emailInput.value.length}.`;
+  }
+
+  emailError.className = "error active";
+}
